36ec73987ffe254137d2dab0aba4c7268f4ea7c5
unknown <jamilafzal255@gmail.com>
2024-04-03 01:50:05 +0500

    adding components and redux

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "./slice/productSlice";
+
+const store = configureStore({
+    reducer: {
+        product:productSlice
+    }
+})
+export default store
