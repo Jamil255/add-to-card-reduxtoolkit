300e8e2f73cdde81205d6ad6bdbd1829d0c38764
unknown <jamilafzal255@gmail.com>
2024-04-03 23:19:53 +0500

    card fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchData } from './store/slice/productSlice'
 import ProductCard from './components/ProductCard'
 import Navabar from './components/Navabar'
 const App = () => {
+  const { loading, products } = useSelector((state) => state.product)
   const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [])
+
   return (
     <>
-      {/* <button onClick={(e) => dispatch(fetchData())}>get data</button> */}
       <Navabar />
+
       <hr />
-      <ProductCard />
+      {loading ? (
+        <h1>loading......</h1>
+      ) : (
+        <div className="  d-flex gap-4  flex-wrap  mx-3">
+          {products?.map((prod) => {
+            return (
+              <div key={prod.id}>
+                <ProductCard product={prod} />
+              </div>
+            )
+          })}
+        </div>
+      )}
     </>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchData } from '../store/slice/productSlice'
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch()
   return (
     <>
       <div className="card" style={{ width: '18rem' }}>
         <img
-          src="../../public/vite.svg"
+          src={product.image}
           width={100}
           height={100}
           className="card-img-top"
         />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+        <div
+          className="card-body"
+          style={{ height: '200px', overflow: 'hidden' }}
+        >
+          <h5 className="card-title">{product.title.slice(0, 40)}...</h5>
+          <p className="card-text">{product.description.slice(0, 150)}..</p>
         </div>
+        <button type="button" className="btn btn-danger">
+          Add To Card
+        </button>
       </div>
     </>
   )
